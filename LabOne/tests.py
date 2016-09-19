cb27748ba37af012e341069689199aaf445835c1
harley1011 <harley.1011@gmail.com>
2016-09-19 15:59:47 -0400

    Adding request_header method to generate the request header line for HTTP request

--- LabOne/tests.py
+++ LabOne/tests.py
@@ -4,8 +4,12 @@ import http
 class TestHttp(unittest.TestCase):
 
     def test_http(self):
-        http.post("http://www.test.com", 80)
+        #http.post("http://www.test.com", 80)
         self.assertTrue(True, True)
+
+    def test_request_header(self):
+        result = http.request_line('GET', 'http://www.test.com', 'HTTP/1.0')
+        print(result)
 
 if __name__ == '__main__':
     unittest.main()
