a9f234184b11a4d2ffbd5399d38cf30fa230376f
David Bastien <david.bastien5@gmail.com>
2016-10-24 14:37:37 -0400

    done parsing

--- ltwo/lib/https.py
+++ ltwo/lib/https.py
@@ -31,16 +31,20 @@ class HTTPServer(object):
                 more_content = False
                 if not data:
                     break
-                elif data.find("\r\n\r\n"):
+                elif data.find(b'\r\n\r\n') != -1:
                     # If we have this then we know we have the status line and all the headers
                     request.extend(data)
+
                     http_request = HttpRequest()
 
-                    more_content = http_request.parse_request(request)
-                    if more_content:
+                    more_content = http_request.parse_request(request, conn)
+                    if not more_content:
                         break
                 else:
                     request.extend(data)
+                    more_content = http_request.add_content(request)
+                    if not more_content:
+                        break
             return http_request
         except:
             conn.close()
@@ -60,15 +64,58 @@ class HttpRequest(object):
         self.headers = {}
         self.message_body = ''
         self.connection = None
+        self.raw_request = None
+        self.request_index = 0
 
     def parse_request(self, request, connection):
         # extract stuff from response
         # see http response for how we parsed the response
-        self.method = 'GET'
+
         self.connection = connection
+
+        self.raw_request = request
+        self.request_index = len(request)
+        request = request.split(b'\r\n\r\n', 1)
+        request_header = request[0].decode('utf-8').split('\r\n', 1)
+        request_body = request[1]
+
+        status_line = request_header.pop(0).split(' ')
+        request_header = request_header[0].split('\r\n')
+        self.method = status_line[0]
+        self.path = status_line[1]
+        self.http_version = status_line[2]
+
+        while len(request_header) > 0:
+            header = request_header.pop(0)
+
+            if header == '':
+                break
+
+            header = header.split(": ", 1)
+            self.headers[header[0]] = header[1]
+
+        try:
+            decode_type = 'utf-8'
+            if 'Content-Type' in self.headers and 'charset=' in self.headers['Content-Type']:
+                decode_type = self.headers['Content-Type'].split('charset=')[1]
+            self.message_body += request_body.decode(decode_type)
+        except:
+            self.message_body += request_body
+
+        if 'Content-Length' in self.headers:
+            return len(self.message_body) < int(self.headers['Content-Length'])
+
         return False
 
     def add_content(self, request):
-        self.message_body += request
+        body = request[self.request_index:]
+        self.request_index = len(request)
+        try:
+            decode_type = 'utf-8'
+            if 'Content-Type' in self.headers and 'charset=' in self.headers['Content-Type']:
+                decode_type = self.headers['Content-Type'].split('charset=')[1]
+            self.message_body += body.decode(decode_type)
+        except:
+            self.message_body += body
 
         return self.headers['Content-Length'] == len(self.message_body)
