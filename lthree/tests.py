82df6a0a6d38289eb8caad938e8d7ca90312b97a
davidbastien5 <david.bastien5@gmail.com>
2016-11-25 11:50:26 -0500

    reduced the delay

--- lthree/tests.py
+++ lthree/tests.py
@@ -26,7 +26,7 @@ class TestHttp(unittest.TestCase):
 
         conn, addr = tcp_listener.accept()
 
-        time.sleep(20)
+        time.sleep(10)
 
         print(conn.send_window)
         print(conn.receive_window)
