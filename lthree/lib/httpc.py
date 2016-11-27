395811665bb777b86ea09c6df057e1fcdcb663b2
harley1011 <harley.1011@gmail.com>
2016-11-27 13:09:47 -0500

    Woring on parsing response

--- lthree/lib/httpc.py
+++ lthree/lib/httpc.py
@@ -85,6 +85,27 @@ class HttpConnection(object):
         finally:
             return response
 
+        try:
+            while True:
+                data = self.tcp_socket.recv_from(4096)
+                if not data:
+                    break
+                elif data.find(b'\r\n\r\n') != -1:
+                    # If we have this then we know we have the status line and all the headers
+                    response.extend(data)
+                    http_response = HttpResponse()
+                    more_content = http_response.parse_request(response)
+                    if not more_content:
+                        break
+                else:
+                    request.extend(data)
+                    more_content = http_request.add_content(request)
+                    if not more_content:
+                        break
+            queue.put(http_request)
+        except:
+            conn.close()
+
 
 class HttpResponse(object):
     def __init__(self, response):
@@ -100,6 +121,7 @@ class HttpResponse(object):
         self.status_code = int(status_line[1])
         self.reason_phrase = status_line[2]
         self.headers = {}
+        self.response_index = 0
 
         while len(response_header) > 0:
             header = response_header.pop(0)
@@ -117,6 +139,57 @@ class HttpResponse(object):
             self.body = response_body.decode(decode_type)
         except:
             self.body = response_body
+
+    def add_content(self, request):
+        body = request[self.request_index:]
+        self.response_index = len(request)
+        try:
+            decode_type = 'utf-8'
+            if 'Content-Type' in self.headers and 'charset=' in self.headers['Content-Type']:
+                decode_type = self.headers['Content-Type'].split('charset=')[1]
+            self.message_body += body.decode(decode_type)
+        except:
+            self.message_body += body
+
+        return self.headers['Content-Length'] == len(self.message_body)
+
+    def parse_request(self, response):
+        # extract stuff from response
+        # see http response for how we parsed the response
+
+        self.raw_response = response
+        self.response_index = len(response)
+        response = response.split(b'\r\n\r\n', 1)
+        request_header = response[0].decode('utf-8').split('\r\n', 1)
+        request_body = response[1]
+
+        status_line = request_header.pop(0).split(' ')
+        request_header = request_header[0].split('\r\n')
+        self.method = status_line[0]
+        self.path = status_line[1]
+        self.http_version = status_line[2]
+
+        while len(request_header) > 0:
+            header = request_header.pop(0)
+
+            if header == '':
+                break
+
+            header = header.split(": ", 1)
+            self.headers[header[0]] = header[1]
+
+        try:
+            decode_type = 'utf-8'
+            if 'Content-Type' in self.headers and 'charset=' in self.headers['Content-Type']:
+                decode_type = self.headers['Content-Type'].split('charset=')[1]
+            self.message_body += request_body.decode(decode_type)
+        except:
+            self.message_body += request_body
+
+        if 'Content-Length' in self.headers:
+            return len(self.message_body) < int(self.headers['Content-Length'])
+
+        return False
 
 
 class InvalidRequest(Exception):
